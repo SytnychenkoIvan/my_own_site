6f70a1ea2c4d262242825bd432c1eb53d36ff568
Ivan Sytnychenko <sytnychenkoivan@gmail.com>
2026-05-30 13:40:09 +0300

    starts make section contacts

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,29 @@
 // import { isMobile } from "./functions.js";
 
 
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', async (e) => {
+	e.preventDefault();
+
+	const data = {
+		name: document.getElementById('name').value,
+		email: document.getElementById('email').value,
+		message: document.getElementById('message').value
+	};
+
+	const response = await fetch('/.netlify/functions/telegram', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	});
+
+	if (response.ok) {
+		alert('Сообщение отправлено!');
+		form.reset();
+	} else {
+		alert('Ошибка');
+	}
+});
